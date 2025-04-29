f79b112012a6695b3953e2e9da01d90abf91b103
junaidsa <94611843+junaidsa@users.noreply.github.com>
2025-04-29 20:24:50 +0500

    fb

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/admin/dashboard',[Homecontroller::class,'dashboard'])->name('dashbo
 
 
 Route::middleware('auth')->group(function () {
+    Route::get('/admin/dashboard',[Homecontroller::class,'dashboard'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
