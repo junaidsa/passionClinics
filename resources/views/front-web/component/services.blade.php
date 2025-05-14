447931b7e744011a5bcd41a79a37a0dd9277a1e3
junaidsa <94611843+junaidsa@users.noreply.github.com>
2025-05-14 15:00:55 +0500

    home

--- resources/views/front-web/component/services.blade.php
+++ resources/views/front-web/component/services.blade.php
@@ -4,9 +4,8 @@
 
     $services = DB::table('services')->where('offer_type', '0')->get();
 
-
-
     // $services = $query->get();
+
 @endphp
 <style>
     .price-badge {
@@ -40,29 +39,19 @@
     }
 </style>
 
-{{-- <div class="row">
+<div class="row">
     @foreach ($services as $s)
         <div class="col-lg-4 col-md-6">
             <div class="service-item wow fadeInUp position-relative">
-                @if ($s->price)
-                    <div class="price-badge">
-                        {{ App::isLocale('ar') ? number_format($s->price, 2) . ' ر.س' : '$' . number_format($s->price, 2) }}
-                    </div>
-                @endif
                 <div class="service-content">
                     <div class="service-content-title">
                         <h2><a href="service-single.html">{{ App::isLocale('ar') ? @$s->title_ar : @$s->title_en }}</a>
                         </h2>
-                        <a href="{{ route('front.service.single', $s->id) }}" class="readmore-btn"><img
-                                src="{{ asset('public') }}/images/arrow-white.svg" alt=""></a>
-                    </div> --}}
-{{-- @if ($s->price)
-                        <p class="service-price fw-bold text-primary mt-2">
-                            {{ App::isLocale('ar') ? number_format($s->price, 2) . ' ر.س' : '$' . number_format($s->price, 2) }}
-                        </p>
-                    @endif --}}
-{{-- <p>{{$s->short_description_en}}</p> --}}
-{{-- <p>{{ App::isLocale('ar') ? @$s->short_description_ar : @$s->short_description_en }}</p>
+                        {{-- <a href="{{ route('front.service.single', $s->id) }}" class="readmore-btn"><img
+                                src="{{ asset('public') }}/images/arrow-white.svg" alt=""></a> --}}
+                    </div>
+                    {{-- <p>{{$s->short_description_en}}</p> --}}
+                    <p>{{ App::isLocale('ar') ? @$s->short_description_ar : @$s->short_description_en }}</p>
                 </div>
                 <div class="service-image">
                     <a href="service-single.html" data-cursor-text="View">
@@ -75,9 +64,9 @@
         </div>
     @endforeach
 
-</div> --}}
+</div>
 
-<div class="row g-4">
+{{-- <div class="row g-4">
     @foreach ($services as $s)
         <div class="col-12 col-md-6 col-lg-4">
             <div class="service-item position-relative shadow-sm rounded p-3 h-100 d-flex flex-column bg-white">
@@ -114,4 +103,4 @@
             </div>
         </div>
     @endforeach
-</div>
+</div> --}}
